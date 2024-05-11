8270d84693e26704231bd0f15849f40302791f01
Rana73 <mahmudrana21@gmail.com>
2024-05-11 23:13:47 +0600

    Update Balance without id and amount

--- banking_system/app/Http/Controllers/TransactionController.php
+++ banking_system/app/Http/Controllers/TransactionController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers;
 
+use App\Models\User;
 use App\Models\Transaction;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\DB;
@@ -52,14 +53,35 @@ class TransactionController extends Controller
 
             DB::beginTransaction();
             $amount = $request->input("amount");
+            $user_id = Auth::user()->id;
+
+            /*lock this account info*/
+            $lockedUser = user::where('id',$user_id)->lockForUpdate()->first();
+            $lockedTransaction = Transaction::where('user_id',$user_id)->lockForUpdate()->get();
+            /*close lock this account info*/
+            
             $data = [
                 'amount' => $amount,
                 'transaction_type' => 'deposit',
-                'user_id' => Auth::user()->id,
+                'user_id' => $user_id,
                 'fee'     => 0,
                 'date' => date("Y-m-d")
             ];
             Transaction::insert($data);
+
+            $deposit_balance = Transaction::where('user_id',$user_id)
+                                ->where('transaction_type','deposit')
+                                ->selectRaw("(sum(IFNULL(amount,0)) - sum(IFNULL(fee,0))) as amount")->first();
+            $withdraw_balance = Transaction::where('user_id',$user_id)
+                                ->where('transaction_type','withdraw')
+                                ->selectRaw("(sum(IFNULL(amount,0)) + sum(IFNULL(fee,0))) as amount")->first();
+            $balance = User::where('id',$user_id)->value('balance');
+
+            $deposit_balance = isset($deposit_balance->amount) ? $deposit_balance->amount : 0 ; 
+            $withdraw_balance = isset($withdraw_balance->amount) ? $withdraw_balance->amount : 0 ; 
+            $current_balance = (floatVal($deposit_balance) + floatVal($balance)) - (floatVal($withdraw_balance));
+
+            User::where('id',$user_id)->update(['balance' => $current_balance]);
             DB::commit();
             return redirect()->back()->with('success','Successfullty Deposited and Balance Updated');
         } catch (\Throwable $th) {
